1fcfc82e7d92f6c3629a4ea79d752c0f80bc56fb
Hb_W <hugobarillasvi@hotmail.com>
2024-02-24 16:10:35 -0600

    junit creación
    crear el test

--- src/test/java/uvg/edu/gt/AppTest.java
+++ src/test/java/uvg/edu/gt/AppTest.java
@@ -3,18 +3,25 @@ package uvg.edu.gt;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    private Calculator calculator = Calculator.getInstance();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testConvertToPostfix() {
+        String infix = "(3 + 4) * (5 - 2)";
+        String expectedPostfix = "34+52-*";
+        assertEquals(expectedPostfix, calculator.convertToPostfix(infix));
     }
+
+    @Test
+    public void testEvaluatePostfix() {
+        String postfix = "34+52-*";
+        int expectedResult = 21;
+        assertEquals(expectedResult, calculator.evaluatePostfix(postfix));
+    }
+
+    // Aquí puedes agregar más métodos de prueba...
 }
